b7c632febf4a2138085e773e75611532e63af72e
SachinVenugopalan30 <sachin.venugopal795@gmail.com>
2024-08-08 08:24:29 +0000

    Added a schedule for daily updates (UNTESTED), fixed issue with keys having decimal values

--- websocket_service.py
+++ websocket_service.py
@@ -10,6 +10,7 @@ import mariadb
 import discord
 import aiohttp
 from discord import Webhook
+import schedule
 
 class WebSocketService:
     def __init__(self):
@@ -21,7 +22,12 @@ class WebSocketService:
         self.db_table = os.getenv('DB_TABLE')
         self.socket_url = os.getenv('SOCKET_URL')
         self.webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
-    
+        self.total_event_count = 0
+        self.listing_create_count = 0
+        self.listing_delete_count = 0
+
+        schedule.every().day.at("14:00").do(self.send_daily_report)
+
     async def webhook_service(self, webhook_type, webhook_title, webhook_message) -> None:
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(self.webhook_url, session=session)
@@ -32,11 +38,30 @@ class WebSocketService:
             else:
                 colour = 0xf9ff00
             embed = discord.Embed(title=webhook_title, description=webhook_message, colour=colour)
+            
+            current_datetime = datetime.now()
+            embed.set_footer(icon_url='https://cdn.discordapp.com/attachments/670639866824097813/1271011402907127838/angry_tort.png?ex=66b5c91c&is=66b4779c&hm=d25c1d6d56aee47c730781c39b70ec6a2a204143513e781120c54576b03b84f8&',
+                            text=current_datetime)
 
             if webhook_type == 'error':
-                await webhook.send(content='<@242688398475657216>', embed=embed, username='Websocket Service')
+                await webhook.send(content=f'<@242688398475657216>', embed=embed, username='Websocket Service')
                 return
             await webhook.send(embed=embed, username='Websocket Service')
+
+    async def status_update_webhook(self, webhook_type, webhook_title, webhook_message) -> None:
+        async with aiohttp.ClientSession() as session:
+            webhook = Webhook.from_url(self.webhook_url, session=session)
+            embed = discord.Embed(title=webhook_title, color=0x249800)
+            embed.add_field(name="Total Listing Create/Update Events", value=self.listing_create_count, inline=True)
+            embed.add_field(name="Total Listing Delete Events", value=self.listing_delete_count, inline=True)
+            embed.add_field(name="Total Events Processed", value=self.listing_create_count + self.listing_delete_count, inline=True)
+
+            current_datetime = datetime.now()
+            
+            embed.set_footer(icon_url='https://cdn.discordapp.com/attachments/670639866824097813/1271011402907127838/angry_tort.png?ex=66b5c91c&is=66b4779c&hm=d25c1d6d56aee47c730781c39b70ec6a2a204143513e781120c54576b03b84f8&',
+                            text=current_datetime)
+            await webhook.send(content=f'<@242688398475657216>', embed=embed, username='Daily Stats')
+            return
 
 
     def killstreak_effects(self, itemPayload) -> None:
@@ -114,11 +139,16 @@ class WebSocketService:
                 'event_type' : event['event'],
                 'event_intent' : event_payload['intent'], # this is either 'sell', 'buy'
                 'steam_id' : event_payload['steamid'],
-                'price_keys' : list_price_keys, # price in keys for the listing
+                'price_keys' : int(list_price_keys), # price in keys for the listing
                 'price_metal' : float(list_price_metal), # price in metal for the listing
                 'is_painted' : int(self.is_painted(event_payload)),
                 'is_spelled' : int(self.is_spelled(event_payload)),
             }
+            if event['event'] == 'listing_update':
+                self.listing_create_count += 1
+            else:
+                self.listing_delete_count += 1
+
             processed_events_list.append(event_info)
         return processed_events_list
 
@@ -144,9 +174,17 @@ class WebSocketService:
             return(True)
         except Exception as e:
             await self.webhook_service("error", "Error!!", f"Error dumping into database! {e}")
+            print(eventsList)
             print(f'Exception occured when trying to insert element {e}')
             return(False)
         
+    async def send_daily_report(self):
+        '''
+        Function to send the daily report to the user via Discord webhook
+        '''
+        await self.status_update_webhook("info", "Daily Report", self.listing_create_count, self.listing_delete_count)
+        self.listing_create_count = 0 # Reset event count at the end of the day
+        self.listing_delete_count = 0
 
     async def main_service(self):
         '''
